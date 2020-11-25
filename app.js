b9d8e3bd0873d017eb43ed474ec76d76af18c7c8
nahyunnn <wartet2@gmail.com>
2020-11-25 16:56:27 +0900

    process/idSearch 생성

--- app.js
+++ app.js
@@ -17,13 +17,8 @@ http.createServer(app).listen(app.get('port'), function(){
 
 
 //라우팅 함수 등록
-router.route('/process/register').post();
+router.route('/process/idSearch').post();
 
-router.route('/process/login').post();
-router.route('/process/reserve/O').post();
-router.route('/process/reserve/I').post();
-router.route('/process/delegate/O').post();
-router.route('/process/delegate/I').post();
 
 
 
