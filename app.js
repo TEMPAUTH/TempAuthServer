a2f9b7bbf00bb66c24375834fe500355fca42cec
nahyunnn <wartet2@gmail.com>
2020-11-25 14:14:41 +0900

    test

---
+++ app.js
@@ -0,0 +1,26 @@
+var express=require('express'), http = require('http');
+
+//익스프레스 객체 생성
+var app = express();
+
+//라우터 객체 생성
+var router = express.Router();
+
+
+//기본 포트를 app 객체 속성으로 설정
+app.set('port', process.env.PORT || 3000);
+
+//express 서버 시작
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('익스프레스 서버를 시작했습니다. :' + app.get('port'));
+});
+
+
+//라우팅 함수 등록
+router.route('/process/register').post();
+
+router.route('/process/login').post();
+router.route('/process/reserve/O').post();
+router.route('/process/reserve/I').post();
+router.route('/process/delegate/O').post();
+router.route('/process/delegate/I').post();
